f40c1cce3673518a17499ae7afa9515702ea0566
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-08-20 17:47:40 +0530

    Update Weather Observation Station 5.sql

--- Weather Observation Station 5.sql
+++ Weather Observation Station 5.sql
@@ -1,3 +1,9 @@
+/* Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
 +-------------+------------+
 | Field       |   Type     |
 +-------------+------------+
@@ -6,7 +12,8 @@
 | STATE       | VARCHAR(2) |
 | LAT_N       | NUMERIC    |
 | LONG_W      | NUMERIC    |
-+-------------+------------+
++-------------+------------+ */
+
 select distinct city from station 
 where left(city,1) in ('a','e','i','o','u') 
 and right(city, 1) in ('a','e','i','o','u')
