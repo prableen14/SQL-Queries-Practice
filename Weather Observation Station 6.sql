00ce6bdfba8fc99d447701e2f725d7ab0a55471c
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-08-20 17:52:10 +0530

    Create Weather Observation Station 6.sql

---
+++ Weather Observation Station 6.sql
@@ -0,0 +1,19 @@
+/*Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE NOT (CITY LIKE 'A%' OR  CITY  LIKE 'E%' OR CITY  LIKE 'I%' OR CITY  LIKE 'O%' OR CITY  LIKE 'U%')
