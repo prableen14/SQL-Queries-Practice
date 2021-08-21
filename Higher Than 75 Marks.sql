90811652c995cc4d65ba9475db446c275ba2b2af
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-08-21 16:34:16 +0530

    Update Higher Than 75 Marks.sql

--- Higher Than 75 Marks.sql
+++ Higher Than 75 Marks.sql
@@ -1,5 +1,15 @@
 /* Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. 
 If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID. 
+
+Sample Output
+
+Ashley
+Julia
+Belvet
+
+Explanation
+
+Only Ashley, Julia, and Belvet have Marks > . If you look at the last three characters of each of their names, there are no duplicates and 'ley' < 'lia' < 'vet'.
 */
 
 SELECT NAME FROM STUDENTS 
