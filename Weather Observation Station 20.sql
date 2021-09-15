0a676a934e46ed001b1412d11809db6f5c1cbe2c
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-09-15 23:24:35 +0530

    Update Weather Observation Station 20.sql

--- Weather Observation Station 20.sql
+++ Weather Observation Station 20.sql
@@ -1,5 +1,9 @@
 /*A median is defined as a number separating the higher half of a data set from the lower half. 
-Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to 4 decimal places. */
+Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to 4 decimal places. 
 
-SELECT ROUND(MEDIAN(Lat_N), 4)   //Oracle soln
+
+Oracle soln:
+*/
+
+SELECT ROUND(MEDIAN(Lat_N), 4)   
 FROM Station;
