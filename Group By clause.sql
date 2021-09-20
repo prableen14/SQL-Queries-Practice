50940cf09b102ddd02f43d79aa6b5f4e760b8ca5
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-09-20 07:12:19 +0530

    Update Group By clause.sql

--- Group By clause.sql
+++ Group By clause.sql
@@ -1,3 +1,7 @@
-/* Write a query to display highest salary department wise and names of employee who is taking that salary. */
+/* Write a query to display highest salary department wise and names of employee who is taking that salary. 
+
+GROUP BY clause rule- We can only write the same attribute with SELECT which is mentioned in with group by clause. 
+But we can write different attributes names with aggregate functions with the SELECT statement.
+*/
 
 Select Ename from Emp where Salary In(Select Max(Salary) from Emp group by Dept)
